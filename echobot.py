9d1ce3067770faada6a7fb1b4cca2ca44c8f8f9e
iabuzayda <156875816+iabuzayda@users.noreply.github.com>
2026-06-03 14:16:34 -0400

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import numpy as np
 
-"""
-with st.chat_message("user"):
-    st.write("Hello 👋")
-"""
+# with st.chat_message("user"):
+#     st.write("Hello 👋")
 
 with st.chat_message("assistant"):
     st.write("Hello human")
