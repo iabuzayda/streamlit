96ee63031e9a99f2b70ddebaf1a763dea63696fe
iabuzayda <156875816+iabuzayda@users.noreply.github.com>
2026-06-03 14:22:27 -0400

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -1,11 +1,20 @@
+# Adding comments
+z='''
+
 import streamlit as st
 import numpy as np
 
-# Adding comments
-z='''
 with st.chat_message("user"):
      st.write("Hello 👋")
-'''
+
 with st.chat_message("assistant"):
     st.write("Hello human")
     st.bar_chart(np.random.randn(30, 3))
+    
+'''
+
+import streamlit as st
+
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
